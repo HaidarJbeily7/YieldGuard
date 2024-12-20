2d3ace83b3668cb7d518aaf00a33e90f8508ea50
Mohammad Safi <the.mohammad.safi@gmail.com>
2024-12-21 00:11:14 +0300

    Wrap application in Near Wallet Provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { WalletProvider } from "./context/WalletProvider";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <WalletProvider>
+    <App />
+  </WalletProvider>
+);
