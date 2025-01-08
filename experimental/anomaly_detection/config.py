aab3d53796f0ee101d15d06464edc275a9286bf1
HaidarJbeily7 <h.jbeily@innopolis.university>
2025-01-08 10:32:24 +0300

    feat: implement anomaly detection with autoencoder

---
+++ experimental/anomaly_detection/config.py
@@ -0,0 +1,13 @@
+class Config:
+    # Model parameters
+    LATENT_DIM = 32
+    INPUT_DIM = 100
+    
+    # Training parameters
+    BATCH_SIZE = 64
+    LEARNING_RATE = 0.001
+    NUM_EPOCHS = 100
+    
+    # Data parameters
+    TRAIN_TEST_SPLIT = 0.8
+    RANDOM_SEED = 42 
