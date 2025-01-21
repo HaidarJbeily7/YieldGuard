1029ffae2d957ccf26b338349efba4d1b67da896
Mohammad Safi <the.mohammad.safi@gmail.com>
2025-01-21 11:12:54 +0300

    create: Total Transaction Count for Activity Section

---
+++ frontend/src/components/Dashboard/YourActivities/TotalTransactions.tsx
@@ -0,0 +1,21 @@
+import { Paper, Text } from "@mantine/core";
+import styles from "./TotalTransactions.module.css";
+
+export function TotalTransactions({ totalTx }: { totalTx: number }) {
+  return (
+    <Paper my="2rem" mr="2rem" withBorder radius="md">
+      <div className={styles.outer}>
+        <div className={styles.dot}></div>
+        <div className={styles.card}>
+          {/* <div className={styles.ray}></div> */}
+          <div className={styles.text}>{totalTx}</div>
+          <Text fw={500}>Transactions</Text>
+          <div className={`${styles.line} ${styles.topl}`}></div>
+          <div className={`${styles.line} ${styles.leftl}`}></div>
+          <div className={`${styles.line} ${styles.bottoml}`}></div>
+          <div className={`${styles.line} ${styles.rightl}`}></div>
+        </div>
+      </div>
+    </Paper>
+  );
+}
