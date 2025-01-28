4835b38ead1a530447494fb25e504af7f3ec82fb
HaidarJbeily7 <h.jbeily@innopolis.university>
2025-01-28 22:10:00 +0300

    feat: add reset password functionality

--- experimental/multitenancy-backend/config/api_urls.py
+++ experimental/multitenancy-backend/config/api_urls.py
@@ -1,14 +1,24 @@
 from users.controllers import UserController
+from core.controllers import PublicCoreController
 from organizations.controllers import OrganizationController
 from ninja_jwt.controller import NinjaJWTDefaultController
 from ninja_extra import NinjaExtraAPI
 from django.conf import settings
+from django_rest_passwordreset.controller import ResetPasswordController
+from scalar_django_ninja import ScalarViewer
 
-api = NinjaExtraAPI(title="Darrbak API", version=settings.VERSION or "1.0.0")
-
+api = NinjaExtraAPI(
+    version="1.0.0",
+    title="Darrbak API",
+    description="API Reference for the Darrbak API",
+    docs=ScalarViewer(),
+    docs_url="/docs",
+)
 
 api.register_controllers(
     NinjaJWTDefaultController,
     UserController,
-    OrganizationController
+    OrganizationController,
+    ResetPasswordController,
+    PublicCoreController,
 )
