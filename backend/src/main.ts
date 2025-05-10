03bf9d31d4b4cdd3955ca6c885d582b514036267
HaidarJbeily7 <h.jbeily@innopolis.university>
2025-05-10 12:17:25 +0300

    chore: update dependencies and modify Swagger server URL in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,13 +8,13 @@ import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('YieldGaurd API')
     .setDescription('API description')
     .setVersion('1.0')
     .addTag('API')
-    .addServer('http://localhost:3000/api')
+    .addServer('http://localhost:3000/')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
